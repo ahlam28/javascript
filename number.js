b8a29d07aa670f027baee2d0b62abee76dbf4665
ahlam28 <55088246+ahlam28@users.noreply.github.com>
2021-06-14 09:13:50 +0300

    Create number.js

---
+++ number.js
@@ -0,0 +1,6 @@
+    BigINT  is a built-in object whose constructor returns a bigint and is used when operations on large data are required.
+    Convert the following values to numbers using any the 4 methods null, undefined, true, false
+    
+    
+    
+    Convert these two strings to number and sum them const string1 ="1000"; const string2 = "2000"
