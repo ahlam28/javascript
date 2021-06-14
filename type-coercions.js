2d132e89be55ab3ae56c4048f476ae8deb31e780
ahlam28 <55088246+ahlam28@users.noreply.github.com>
2021-06-14 19:48:23 +0300

    Create type-coercions.js

---
+++ type-coercions.js
@@ -0,0 +1,3 @@
+Assignment
+// 1. What are the use cases for each of the String conversion methods? Are there any performance differences
+// 2. How would you use Number.parseInt or parseInt - What's the difference between them and parseFloat and Number.parseFloat()
