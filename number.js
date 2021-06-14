9d78e434a5276c636968fb0e4828a54a5da5d03b
ahlam28 <55088246+ahlam28@users.noreply.github.com>
2021-06-14 19:46:14 +0300

    Update number.js

--- number.js
+++ number.js
@@ -1,6 +1,34 @@
-    BigINT  is a built-in object whose constructor returns a bigint and is used when operations on large data are required.
+    BigINT  is a built-in object whose constructor returns a bigint and const string1="1000";   // declare and assigning string1 to 1000
+const string2="2000";    // declare and assigning string2 to 2000
+// 1000 and 2000 are string, so i need to change them to Numbers
+console.log(string1);   // you'll get out put string 1000
+console.log(string2);    // you'll get out put string 2000
+ // then you change string into numbers and sum them
+console.log(Number(string1)+Number(string2));  // output is 3000
+ // Also another method you can use parseFloat
+  const string3= "100 cows are dead";
+  const string4= "10";
+  console.log(string4)
+  console.log(string3)
+  console.log(Number.parseFloa(string3));    //// output will be 100 because it only passes number 
+console.log(Number.parseFloat(string4)*Number.parseFloat(string3));  // out put will be 1000is used when operations on large data are required.
     Convert the following values to numbers using any the 4 methods null, undefined, true, false
     
     
     
     Convert these two strings to number and sum them const string1 ="1000"; const string2 = "2000"
+    
+    const string1="1000";   // declare and assigning string1 to 1000
+const string2="2000";    // declare and assigning string2 to 2000
+// 1000 and 2000 are string, so i need to change them to Numbers
+console.log(string1);   // you'll get out put string 1000
+console.log(string2);    // you'll get out put string 2000
+ // then you change string into numbers and sum them
+console.log(Number(string1)+Number(string2));  // output is 3000
+ // Also another method you can use parseFloat
+  const string3= "100 cows are dead";
+  const string4= "10";
+  console.log(string4)
+  console.log(string3)
+  console.log(Number.parseFloa(string3));    //// output will be 100 because it only passes number 
+console.log(Number.parseFloat(string4)*Number.parseFloat(string3));  // out put will be 1000
